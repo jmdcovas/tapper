bcd43836ec63b2ad37bc383f233c35f0d0ddd3a4
João Covas <jmdcovas@MBP-de-Joao-2.lan>
2015-10-28 01:07:28 +0000

    finished

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -9,6 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    @IBAction func onCoinTapped(sender: UIButton!){
+        currentTaps++
+        updateTapsLbl()
+        if isGameOver() {
+            restartGame()
+        }
+    }
+
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
+        
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+            logoImg.hidden = true
+            howManyTapsTxt.hidden = true
+            playBtn.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLbl()
+            
+        }
+    
+    }
+    
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoImg.hidden = false
+        howManyTapsTxt.hidden = false
+        playBtn.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
